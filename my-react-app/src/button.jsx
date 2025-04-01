12da961fdccbdf0ef6795a0fe0d1124fd98f3c51
AnuragMali333 <anuragmali654@gmail.com>
2025-04-01 12:05:20 +0530

    remove unused files and components from basic and vite react projects

--- my-react-app/src/button.jsx
+++
@@ -1,9 +0,0 @@
-function Button(props) {
-  function handleClick() {
-    alert(props.message); // Show a dynamic message
-  }
-
-  return <button onClick={handleClick}>{props.label}</button>;
-}
-
-export default Button;
--- my-react-app/src/greeting.jsx
+++
@@ -1,5 +0,0 @@
-function Greeting(props){
-  return <h2>Hello{props.name}! Welcome to React Components</h2>
-}
-
-export default Greeting
